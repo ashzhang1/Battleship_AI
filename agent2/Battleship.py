0b97e1524b5e18e8d48db6b4d3191d5a678cdf22
ashzhang1 <ashzhang1@gmail.com>
2023-09-16 16:20:43 +1000

    better workflow

--- agent2/Battleship.py
+++ agent2/Battleship.py
@@ -1,74 +1,119 @@
 import random
 import numpy as np
 
+
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
-    # x = random.randint(1,10)
-    # y = random.randint(1,10)
-    # return [x,y], storage
+    global heatMap, allStates, missedShots
     
-    if round > 1:
+    if round == 1:
+        allStates = generateAllBoardStates()
+        heatMap = generateHeatMap(allStates)
+        return pickMove(heatMap), storage
+    
+    # Records missed shot coordinate
+    if not p1PrevHit:
+        missedShots.append(p1ShotSeq[-1])
 
-        # remove all boards from allBoardStates that do not fit the shot seqeunce.
-        updateBoards(p1PrevHit, p1ShotSeq)
+    heatMap = updateHeatMap(p1PrevHit, p1ShotSeq[-1], heatMap)
+    
+    if not zerosHeatMap(heatMap):  
+        return pickMove(heatMap), storage
+
+    allStates = generateAllBoardStates()
+    allStates = filterStates(allStates, missedShots)
+    heatMap = generateHeatMap(allStates)
+    heatMap = setZeros(heatMap, p1ShotSeq)
+    return pickMove(heatMap)
+    
+
+def updateHeatMap(hit, lastShot, heatMap):
+    '''
+    Filter out invalid states based on the move and sum all remaining states
+    '''
+    pass  
+
+def zerosHeatMap(heatMap):
+    
+    pass
+
+def generateAllBoardStates():
+    allStates = np.empty((0,100))
+    ships = [5, 3, 3, 2, 2]
+    for ship in ships:
+        allStates = np.concatenate((allStates, generateBoardStates(ship)), 0)
+
+    return allStates
+
+def filterStates(allStates, missedShots):
+    '''
+    Set the cells to zero based on shot sequence
+    '''
+    pass
+
+def generateHeatMap(allStates):
+    '''
+    Sum all states
+    '''
+    pass
+
+
+def setZeros(heatMap, p1ShotSeq):
+    '''
+    Set the cells to zero based on shot sequence
+    '''
+    pass
+
+def pickMove(heatMap):
+    '''
+    pick a cell with the highest value
+    '''
+    pass
+
+def generateBoardStates(shipSize):
+    board_size = 10
+    ship_sizes = [shipSize]
+    board_states = []
+    
+    def is_valid_placement(board, size, x, y, direction):
+        if direction == 0:
+            return x + size <= board_size and np.all(board[y, x:x+size] == 0)
+        elif direction == 1:
+            return y + size <= board_size and np.all(board[y:y+size, x] == 0)
+        return False
+    
+    def place_ship(board, size, x, y, direction):
+        if direction == 0:
+            board[y, x:x+size] = 1
+        elif direction == 1:
+            board[y:y+size, x] = 1
+    
+    def remove_ship(board, size, x, y, direction):
+        if direction == 0:
+            board[y, x:x+size] = 0
+        elif direction == 1:
+            board[y:y+size, x] = 0
+    
+    def generate(board, remaining_ships):
+        if not remaining_ships:
+            board_states.append(np.ravel(board.copy()))
+            return
         
-    updateProbabilities()
+        ship_size = remaining_ships[0]
+        
+        for x in range(board_size):
+            for y in range(board_size):
+                for direction in [0, 1]:
+                    if is_valid_placement(board, ship_size, x, y, direction):
+                        place_ship(board, ship_size, x, y, direction)
+                        generate(board, remaining_ships[1:])
+                        remove_ship(board, ship_size, x, y, direction)
     
-    return getMove(), storage 
-
-
-def generateBoardStates():
-    # Implement an algorithm to generate all possible states
-    return np.empty
-
-
-def updateBoards(prevHit, shotSeq):
-    shot = shotSeq[-1]
-
-    # Convert shot position to 1D-array index
-    shotIndex = (shot[0] - 1) * 10 + shot[1] - 1
+    empty_board = np.zeros((board_size, board_size), dtype=int)
+    generate(empty_board, ship_sizes)
     
-    boardValidity = list()
+    return np.array(board_states)
 
-    # Update the list of valid placements
-    for board in allBoardStates:
-        cell = board[shotIndex]
-
-        # Check if the cell matches the previous hit
-        if(prevHit and cell != 1):
-            boardValidity.append(False)
-        elif(not prevHit and cell != 0):
-            boardValidity.append(False)
-        else:
-            boardValidity.append(True)
-
-    allBoardStates = allBoardStates[boardValidity]
-
-# Generates probability density map
-def updateProbabilities():
-    map = np.zeros(100)
-    for board in allBoardStates:
-        map += board
-    probabilityDensityMap = map
-
-# Converts 1D-array index into Battleship game index
-def getMove():
-    index = np.argmax(probabilityDensityMap)
-    #Handle tie
-    x = int((index / 10)) + 1
-    y = index % 10 + 1
-    return [x, y]
-
-allBoardStates = generateBoardStates()
-probabilityDensityMap = np.zeros(100)
-
-
-'''
-Tasks:
-1. Generate all valid boards --> ONLY done once.
-    Figure out an algorithm to find all possible placements
-2. Adding all cells of all the boards together --> ONLY done once.
-3. Choose move based on the cell with highest count.
-4a. Remove boards that don't fit the result from p1PrevHits.
-    4b. Recalculate the sum of each cell of all the valid boards by substracting the result from 3a.
-5. Repeat step 3 - 4.
-'''
+#START:
+allStates = None
+heatMap = None
+missedShots = list()
